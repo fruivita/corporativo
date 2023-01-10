a4c53d51e1211c18fb2bcc5f12b14e2992d6fc54
FCNO <fcno.dev@gmail.com>
2023-01-10 10:27:50 -0300

    refactor(core): importação do usuário emite eventos
    - mudança de cargo;
    - mudança de função;
    - mudança de lotação.

--- src/Importadores/ImportadorUsuario.php
+++ src/Importadores/ImportadorUsuario.php
@@ -2,17 +2,22 @@
 
 namespace FruiVita\Corporativo\Importadores;
 
+use FruiVita\Corporativo\Events\CargoUsuarioAlterado;
+use FruiVita\Corporativo\Events\FuncaoConfiancaUsuarioAlterada;
+use FruiVita\Corporativo\Events\LotacaoUsuarioAlterada;
 use FruiVita\Corporativo\Models\Usuario;
+use FruiVita\Corporativo\Trait\Eventos;
 use Illuminate\Support\Collection;
 
 final class ImportadorUsuario extends ImportadorBase
 {
+    use Eventos;
+
     /**
      * {@inheritdoc}
      */
     protected $rules = [
         'matricula' => ['bail', 'required', 'string', 'max:20'],
-        'username' => ['bail', 'required', 'string', 'max:20'],
         'email' => ['bail', 'required', 'string', 'email:strict'],
         'nome' => ['bail', 'required', 'string', 'max:255'],
         'lotacao_id' => ['bail', 'required', 'integer', 'exists:lotacoes,id'],
@@ -33,7 +38,7 @@ final class ImportadorUsuario extends ImportadorBase
     /**
      * {@inheritdoc}
      */
-    protected $atualizar_campos = ['username', 'email', 'nome', 'lotacao_id', 'cargo_id', 'funcao_confianca_id'];
+    protected $atualizar_campos = ['email', 'nome', 'lotacao_id', 'cargo_id', 'funcao_confianca_id'];
 
     /**
      * Create new class instance.
@@ -50,7 +55,6 @@ final class ImportadorUsuario extends ImportadorBase
     {
         return [
             'matricula' => $no->getAttribute('matricula') ?: null,
-            'username' => $no->getAttribute('sigla') ?: null,
             'email' => $no->getAttribute('email') ?: null,
             'nome' => $no->getAttribute('nome') ?: null,
             'lotacao_id' => $no->getAttribute('lotacao') ?: null,
@@ -61,13 +65,79 @@ final class ImportadorUsuario extends ImportadorBase
 
     /**
      * {@inheritdoc}
+     *
+     * Emite os eventos:
+     * - CargoUsuarioAlterado se houver mudança no cargo;
+     * - FuncaoConfiancaUsuarioAlterada se houver mudança na função;
+     * - LotacaoUsuarioAlterada se houver mudança na lotação.
      */
     protected function salvar(Collection $validados)
     {
+        if ($this->emitirEventos()) {
+            Usuario::query()
+                ->whereIn('matricula', $validados->pluck('matricula'))
+                ->lazy()
+                ->each(function (Usuario $usuario) use ($validados) {
+                    $novo = $validados->firstWhere('matricula', $usuario->matricula);
+
+                    $this->mudancaCargo($usuario, data_get($novo, 'cargo_id'));
+                    $this->mudancaFuncao($usuario, data_get($novo, 'funcao_confianca_id'));
+                    $this->mudancaLotacao($usuario, data_get($novo, 'lotacao_id'));
+                });
+        }
+
         Usuario::upsert(
             $validados->toArray(),
             $this->unique,
             $this->atualizar_campos
+        );
+    }
+
+    /**
+     * Emite evento se houver mudança no cargo do usuário.
+     *
+     * @param \FruiVita\Corporativo\Models\Usuario
+     * @param int|null $cargo_novo id do cargo novo
+     */
+    private function mudancaCargo(Usuario $usuario, int $cargo_novo = null)
+    {
+        CargoUsuarioAlterado::dispatchIf(
+            $usuario->cargo_id !== $cargo_novo,
+            $usuario->id,
+            $usuario->cargo_id,
+            $cargo_novo
+        );
+    }
+
+    /**
+     * Emite evento se houver mudança na função de confiança do usuário.
+     *
+     * @param \FruiVita\Corporativo\Models\Usuario
+     * @param int|null $funcao_nova id do funcao nova
+     */
+    private function mudancaFuncao(Usuario $usuario, int $funcao_nova = null)
+    {
+        FuncaoConfiancaUsuarioAlterada::dispatchIf(
+            $usuario->funcao_confianca_id !== $funcao_nova,
+            $usuario->id,
+            $usuario->funcao_confianca_id,
+            $funcao_nova
+        );
+    }
+
+    /**
+     * Emite evento se houver mudança na lotação do usuário.
+     *
+     * @param \FruiVita\Corporativo\Models\Usuario
+     * @param int|null $lotacao_nova id do lotacao nova
+     */
+    private function mudancaLotacao(Usuario $usuario, int $lotacao_nova = null)
+    {
+        LotacaoUsuarioAlterada::dispatchIf(
+            $usuario->lotacao_id !== $lotacao_nova,
+            $usuario->id,
+            $usuario->lotacao_id,
+            $lotacao_nova
         );
     }
 }
--- tests/Feature/Importadores/ImportadorUsuarioTest.php
+++ tests/Feature/Importadores/ImportadorUsuarioTest.php
@@ -5,11 +5,18 @@
  * @see https://laravel.com/docs/9.x/mocking
  */
 
+use FruiVita\Corporativo\Events\CargoUsuarioAlterado;
+use FruiVita\Corporativo\Events\FuncaoConfiancaUsuarioAlterada;
+use FruiVita\Corporativo\Events\LotacaoUsuarioAlterada;
 use FruiVita\Corporativo\Importadores\ImportadorCargo;
 use FruiVita\Corporativo\Importadores\ImportadorFuncaoConfianca;
 use FruiVita\Corporativo\Importadores\ImportadorLotacao;
 use FruiVita\Corporativo\Importadores\ImportadorUsuario;
+use FruiVita\Corporativo\Models\Cargo;
+use FruiVita\Corporativo\Models\FuncaoConfianca;
+use FruiVita\Corporativo\Models\Lotacao;
 use FruiVita\Corporativo\Models\Usuario;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Log;
 
 // Falhas
@@ -24,7 +31,7 @@ test('cria os logs para cada usuário inválido', function () {
 
     Log::shouldHaveReceived('log')
         ->withArgs(fn ($level, $message) => $level === 'warning' && $message === __('Validação falhou'))
-        ->times(17);
+        ->times(14);
 
     expect(Usuario::count())->toBe(5);
 });
@@ -47,4 +54,88 @@ test('importa os usuários do arquivo corporativo', function () {
 
     expect($usuarios)->toHaveCount(5)
         ->and($usuarios->pluck('nome'))->toMatchArray(['Pessoa 1', 'Pessoa 2', 'Pessoa 3', 'Pessoa 4', 'Pessoa 5']);
+});
+
+test('emite evento CargoUsuarioAlterado se for identificada alteração no cargo do usuário durante a importação', function () {
+    config(['corporativo.eventos' => true]);
+
+    Event::fake();
+
+    $cargo_anterior = Cargo::factory()->create(['id' => 5000]);
+
+    $usuario = Usuario::factory()->create([
+        'matricula' => '22222',
+        'cargo_id' => $cargo_anterior->id,
+    ]);
+
+    ImportadorCargo::make()->importar($this->arquivo);
+    ImportadorFuncaoConfianca::make()->importar($this->arquivo);
+    ImportadorLotacao::make()->importar($this->arquivo);
+    ImportadorUsuario::make()->importar($this->arquivo);
+
+    $usuario->refresh();
+
+    Event::assertDispatched(function (CargoUsuarioAlterado $event) use ($cargo_anterior, $usuario) {
+        expect($event->usuario)->toBe($usuario->id)
+            ->and($event->cargo_anterior)->toBe($cargo_anterior->id)
+            ->and($event->cargo_atual)->toBe($usuario->cargo_id);
+
+        return true;
+    });
+});
+
+test('emite evento FuncaoConfiancaUsuarioAlterada se for identificada alteração na função de confiança do usuário durante a importação', function () {
+    config(['corporativo.eventos' => true]);
+
+    Event::fake();
+
+    $funcao_anterior = FuncaoConfianca::factory()->create(['id' => 5000]);
+
+    $usuario = Usuario::factory()->create([
+        'matricula' => '44444',
+        'funcao_confianca_id' => $funcao_anterior->id,
+    ]);
+
+    ImportadorCargo::make()->importar($this->arquivo);
+    ImportadorFuncaoConfianca::make()->importar($this->arquivo);
+    ImportadorLotacao::make()->importar($this->arquivo);
+    ImportadorUsuario::make()->importar($this->arquivo);
+
+    $usuario->refresh();
+
+    Event::assertDispatched(function (FuncaoConfiancaUsuarioAlterada $event) use ($funcao_anterior, $usuario) {
+        expect($event->usuario)->toBe($usuario->id)
+            ->and($event->funcao_anterior)->toBe($funcao_anterior->id)
+            ->and($event->funcao_atual)->toBe($usuario->funcao_confianca_id);
+
+        return true;
+    });
+});
+
+test('emite evento LotacaoUsuarioAlterada se for identificada alteração na lotação do usuário durante a importação', function () {
+    config(['corporativo.eventos' => true]);
+
+    Event::fake();
+
+    $lotacao_anterior = Lotacao::factory()->create(['id' => 5000]);
+
+    $usuario = Usuario::factory()->create([
+        'matricula' => '44444',
+        'lotacao_id' => $lotacao_anterior->id,
+    ]);
+
+    ImportadorCargo::make()->importar($this->arquivo);
+    ImportadorFuncaoConfianca::make()->importar($this->arquivo);
+    ImportadorLotacao::make()->importar($this->arquivo);
+    ImportadorUsuario::make()->importar($this->arquivo);
+
+    $usuario->refresh();
+
+    Event::assertDispatched(function (LotacaoUsuarioAlterada $event) use ($lotacao_anterior, $usuario) {
+        expect($event->usuario)->toBe($usuario->id)
+            ->and($event->lotacao_anterior)->toBe($lotacao_anterior->id)
+            ->and($event->lotacao_atual)->toBe($usuario->lotacao_id);
+
+        return true;
+    });
 });
