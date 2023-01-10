e31580b274c636ed55233e575a9fa3eeaa0234ab
FCNO <fcno.dev@gmail.com>
2023-01-10 13:12:06 -0300

    refactor: remove id do template pessoa

--- tests/template/Corporativo.php
+++ tests/template/Corporativo.php
@@ -74,39 +74,39 @@ return <<<'XML'
     </lotacoes>
     <pessoas>
          <!-- Pessoas Válidas -->
-        <pessoa id="1" nome="Pessoa 1" matricula="11111" email="p1@p1.com" cargo="1" lotacao="2" funcaoConfianca="1"/>
-        <pessoa id="2" nome="Pessoa 2" matricula="22222" email="p2@p2.com" cargo="1" lotacao="2" funcaoConfianca="2"/>
-        <pessoa id="3" nome="Pessoa 3" matricula="33333" email="p3@p3.com" cargo="1" lotacao="3" funcaoConfianca="2"/>
-        <pessoa id="4" nome="Pessoa 4" matricula="44444" email="p4@p4.com" cargo="2" lotacao="3" funcaoConfianca=""/>
-        <pessoa id="5" nome="Pessoa 5" matricula="55555" email="p5@p5.com" cargo="2" lotacao="3" funcaoConfianca=""/>
+        <pessoa nome="Pessoa 1" matricula="11111" email="p1@p1.com" cargo="1" lotacao="2" funcaoConfianca="1"/>
+        <pessoa nome="Pessoa 2" matricula="22222" email="p2@p2.com" cargo="1" lotacao="2" funcaoConfianca="2"/>
+        <pessoa nome="Pessoa 3" matricula="33333" email="p3@p3.com" cargo="1" lotacao="3" funcaoConfianca="2"/>
+        <pessoa nome="Pessoa 4" matricula="44444" email="p4@p4.com" cargo="2" lotacao="3" funcaoConfianca=""/>
+        <pessoa nome="Pessoa 5" matricula="55555" email="p5@p5.com" cargo="2" lotacao="3" funcaoConfianca=""/>
         <!-- Pessoas Inválidas -->
         <!-- Sem o nome -->
-        <pessoa id="6" matricula="66666" email="p6@p6.com" cargo="1" lotacao="2" funcaoConfianca=""/>
+        <pessoa matricula="66666" email="p6@p6.com" cargo="1" lotacao="2" funcaoConfianca=""/>
         <!-- Nome nulo/vazio -->
-        <pessoa id="7" nome="" matricula="77777" email="p7@p7.com" cargo="1" lotacao="2" funcaoConfianca=""/>
+        <pessoa nome="" matricula="77777" email="p7@p7.com" cargo="1" lotacao="2" funcaoConfianca=""/>
         <!-- Nome com mais de 255 caracteres -->
-        <pessoa id="8" nome="F8WRcUYFTJKuA0ZJva5MX5FX7mnMeXqKVHHDsUqfs6nnGjWVi33A0AfziN8Z9ALD2GiQEgltl9Ucd45rs2TSA2jvqeLT2t60X5S3O4lt8j9ZURmSPm1LBvLXWcBmh4vLjORDuSH50SBcwzcZbaTTbxFqZI5k9LOqcw5VlRtqCox44sAFXKkKVms9KvnL9ltmvtRp63JxqpWrp81TuYcnXuPc2MALKWO9Dwxc8pxP6XWKsJ5M3qYVgib8OIEc00O4" matricula="77777" email="p7@p7.com" cargo="1" lotacao="2" funcaoConfianca=""/>
+        <pessoa nome="F8WRcUYFTJKuA0ZJva5MX5FX7mnMeXqKVHHDsUqfs6nnGjWVi33A0AfziN8Z9ALD2GiQEgltl9Ucd45rs2TSA2jvqeLT2t60X5S3O4lt8j9ZURmSPm1LBvLXWcBmh4vLjORDuSH50SBcwzcZbaTTbxFqZI5k9LOqcw5VlRtqCox44sAFXKkKVms9KvnL9ltmvtRp63JxqpWrp81TuYcnXuPc2MALKWO9Dwxc8pxP6XWKsJ5M3qYVgib8OIEc00O4" matricula="77777" email="p7@p7.com" cargo="1" lotacao="2" funcaoConfianca=""/>
         <!-- Sem cargo -->
-        <pessoa id="12" nome="Pessoa 12" matricula="12121" email="p12@p12.com" lotacao="2" funcaoConfianca=""/>
+        <pessoa nome="Pessoa 12" matricula="12121" email="p12@p12.com" lotacao="2" funcaoConfianca=""/>
         <!-- Cargo nulo -->
-        <pessoa id="13" nome="Pessoa 13" matricula="13131" email="p13@p13.com" cargo="" lotacao="2" funcaoConfianca=""/>
+        <pessoa nome="Pessoa 13" matricula="13131" email="p13@p13.com" cargo="" lotacao="2" funcaoConfianca=""/>
         <!-- Cargo inexistente -->
-        <pessoa id="14" nome="Pessoa 14" matricula="14141" email="p14@p14.com" cargo="1000" lotacao="2" funcaoConfianca=""/>
+        <pessoa nome="Pessoa 14" matricula="14141" email="p14@p14.com" cargo="1000" lotacao="2" funcaoConfianca=""/>
         <!-- Sem lotação -->
-        <pessoa id="15" nome="Pessoa 15" matricula="15151" email="p15@p15.com" cargo="1" funcaoConfianca=""/>
+        <pessoa nome="Pessoa 15" matricula="15151" email="p15@p15.com" cargo="1" funcaoConfianca=""/>
         <!-- Lotação nula -->
-        <pessoa id="16" nome="Pessoa 16" matricula="16161" email="p16@p16.com" cargo="1" lotacao="" funcaoConfianca=""/>
+        <pessoa nome="Pessoa 16" matricula="16161" email="p16@p16.com" cargo="1" lotacao="" funcaoConfianca=""/>
         <!-- Lotação inexistente -->
-        <pessoa id="17" nome="Pessoa 17" matricula="17171" email="p17@p17.com" cargo="1" lotacao="1000" funcaoConfianca=""/>
+        <pessoa nome="Pessoa 17" matricula="17171" email="p17@p17.com" cargo="1" lotacao="1000" funcaoConfianca=""/>
         <!-- Função inexistente -->
-        <pessoa id="18" nome="Pessoa 18" matricula="18181" email="p18@p18.com" cargo="1" lotacao="2" funcaoConfianca="1000"/>
+        <pessoa nome="Pessoa 18" matricula="18181" email="p18@p18.com" cargo="1" lotacao="2" funcaoConfianca="1000"/>
         <!-- Matrícula nula/vazia -->
-        <pessoa id="19" nome="Pessoa 19" matricula="" email="p19@p19.com" cargo="1" lotacao="2" funcaoConfianca=""/>
+        <pessoa nome="Pessoa 19" matricula="" email="p19@p19.com" cargo="1" lotacao="2" funcaoConfianca=""/>
         <!-- Matrícula com mais de 20 caracteres -->
-        <pessoa id="20" nome="Pessoa 20" matricula="TJKuA0ZJva5MX5FX7mnMe" email="p20@p20.com" cargo="1" lotacao="2" funcaoConfianca=""/>
+        <pessoa nome="Pessoa 20" matricula="TJKuA0ZJva5MX5FX7mnMe" email="p20@p20.com" cargo="1" lotacao="2" funcaoConfianca=""/>
         <!-- Email inválido -->
-        <pessoa id="21" nome="Pessoa 21" matricula="21212" email="p21@p21" cargo="1" lotacao="2" funcaoConfianca=""/>
-        <pessoa id="22" nome="Pessoa 22" matricula="22022" email="p22p21" cargo="1" lotacao="2" funcaoConfianca=""/>
+        <pessoa nome="Pessoa 21" matricula="21212" email="p21@p21" cargo="1" lotacao="2" funcaoConfianca=""/>
+        <pessoa nome="Pessoa 22" matricula="22022" email="p22p21" cargo="1" lotacao="2" funcaoConfianca=""/>
     </pessoas>
 </base>
 XML;
